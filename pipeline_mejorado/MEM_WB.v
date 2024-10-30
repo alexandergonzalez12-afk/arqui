957de12279bd52084f7d92eb77f25605e0a197d3
bryanfigueroa7 <bryan.figueroa7@upr.edu>
2024-10-30 16:59:35 -0400

    Un intento mas antes de rendirse

---
+++ pipeline_mejorado/MEM_WB.v
@@ -0,0 +1,17 @@
+
+`timescale 1ns / 1ns
+
+module MEM_WB (
+    input clk,
+    input reset,
+    input [31:0] instr_in,
+    output reg [31:0] instr_out
+);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            instr_out <= 32'b0;
+        else
+            instr_out <= instr_in;
+    end
+endmodule
